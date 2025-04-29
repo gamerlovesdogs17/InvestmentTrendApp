7b48ba8374e68cfbe35ab4c058f57d716dd42395
gamerlovesdogs17 <145847426+gamerlovesdogs17@users.noreply.github.com>
2025-04-29 17:26:55 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-```python
 import streamlit as st
 import yfinance as yf
 import pandas as pd
@@ -158,4 +157,3 @@ if st.session_state.running:
 
     # Auto refresh
     st.experimental_rerun()
-```
